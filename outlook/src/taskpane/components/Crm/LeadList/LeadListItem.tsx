53a0ff150f9da71bbf4341986d9d33f1fab4cf4f
std-odoo <std@odoo.com>
2021-09-22 11:08:21 +0200

    [REF] outlook: simplify the component "LeadListItem"
    Purpose
    =======
    Simplify the code of the component "LeadListItem" by using async / await
    instead of promise and use more modern syntax.
    
    The "collapsed" logic should be on the CollapseSection component and
    not on the LeadSection component.
    
    Task-2601837

--- outlook/src/taskpane/components/Crm/LeadList/LeadListItem.tsx
+++ outlook/src/taskpane/components/Crm/LeadList/LeadListItem.tsx
@@ -15,18 +15,14 @@ type LeadsListItemProps = {
 class LeadListItem extends React.Component<LeadsListItemProps, {}> {
     openInOdoo = () => {
         const cids = this.context.getUserCompaniesString();
-        let url =
+        const url =
             api.baseURL +
             `/web#id=${this.props.lead.id}&model=crm.lead&view_type=form${cids}`;
         window.open(url, '_blank');
     };
 
     render() {
-        let expectedRevenueString = _t('%(expected_revenue)s at %(probability)s%', {
-            expected_revenue: this.props.lead.expectedRevenue,
-            probability: this.props.lead.probability,
-        });
-
+        let expectedRevenueString;
         if (this.props.lead.recurringPlan) {
             expectedRevenueString = _t(
                 '%(expected_revenue)s + %(recurring_revenue)s %(recurring_plan)s at %(probability)s%',
@@ -37,18 +33,21 @@ class LeadListItem extends React.Component<LeadsListItemProps, {}> {
                     probability: this.props.lead.probability,
                 },
             );
+        } else {
+            expectedRevenueString = _t('%(expected_revenue)s at %(probability)s%', {
+                expected_revenue: this.props.lead.expectedRevenue,
+                probability: this.props.lead.probability,
+            });
         }
-
-        let expectedRevenuesText = (
-            <div className="lead-list-item-revenue-text">{expectedRevenueString}</div>
-        );
 
         return (
             <div className="list-item-root-container" onClick={this.openInOdoo}>
                 <div className="list-item-container">
                     <div className="list-item-info-container">
                         <div className="list-item-title-text">{this.props.lead.name}</div>
-                        {expectedRevenuesText}
+                        <div className="lead-list-item-revenue-text">
+                            {expectedRevenueString}
+                        </div>
                     </div>
                     <Logger
                         resId={this.props.lead.id}
